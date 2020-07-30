abc9bf31ec374632890281cda511a54830187527
geocode-matt <mlindenx@gmail.com>
2020-07-30 09:44:45 -0500

    front end talking to back

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
@@ -112,6 +113,14 @@ function filterByQuery(query, animalsArray) {
 
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
   });
 
   app.listen(PORT, () => {
